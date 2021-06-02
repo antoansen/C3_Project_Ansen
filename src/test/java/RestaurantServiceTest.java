e74dce3eff88b3021864faa7abd3a91da8b40b58
antoansen <antoansen@gmail.com>
2021-06-01 22:16:07 -0400

    Initial commit

---
+++ src/test/java/RestaurantServiceTest.java
@@ -0,0 +1,58 @@
+import org.junit.jupiter.api.*;
+
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class RestaurantServiceTest {
+
+    RestaurantService service = new RestaurantService();
+    Restaurant restaurant;
+
+    LocalTime openingTime = LocalTime.parse("10:30:00");
+    LocalTime closingTime = LocalTime.parse("22:00:00");
+    //REFACTOR ALL THE REPEATED LINES OF CODE
+
+    //>>>>>>>>>>>>>>>>>>>>>>SEARCHING<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    @Test
+    public void searching_for_existing_restaurant_should_return_expected_restaurant_object() throws restaurantNotFoundException {
+        restaurant = service.addRestaurant("Tester","Chennai",openingTime,closingTime);
+        Restaurant expectedRestaurant = service.findRestaurantByName("Tester");
+        assertEquals(restaurant, expectedRestaurant);
+    }
+
+    @Test
+    public void searching_for_non_existing_restaurant_should_throw_exception() {
+        restaurant = service.addRestaurant("Tester 2","Chennai",openingTime,closingTime);
+        assertThrows(restaurantNotFoundException.class,()->service.findRestaurantByName("Invalid restaurant name"));
+    }
+    //<<<<<<<<<<<<<<<<<<<<SEARCHING>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+    //>>>>>>>>>>>>>>>>>>>>>>ADMIN: ADDING & REMOVING RESTAURANTS<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    @Test
+    public void remove_restaurant_should_reduce_list_of_restaurants_size_by_1() throws restaurantNotFoundException {
+        restaurant = service.addRestaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+        int initialNumberOfRestaurants = service.getRestaurants().size();
+        service.removeRestaurant("Amelie's cafe");
+        assertEquals(initialNumberOfRestaurants-1, service.getRestaurants().size());
+    }
+
+    @Test
+    public void removing_restaurant_that_does_not_exist_should_throw_exception() throws restaurantNotFoundException {
+        restaurant = service.addRestaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        assertThrows(restaurantNotFoundException.class,()->service.removeRestaurant("Pantry d'or"));
+    }
+
+    @Test
+    public void add_restaurant_should_increase_list_of_restaurants_size_by_1() throws restaurantNotFoundException {
+        restaurant = service.addRestaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        int initialNumberOfRestaurants = service.getRestaurants().size();
+        service.addRestaurant("Pumpkin Tales","Chennai", openingTime,closingTime);
+        assertEquals(initialNumberOfRestaurants + 1,service.getRestaurants().size());
+    }
+    //<<<<<<<<<<<<<<<<<<<<ADMIN: ADDING & REMOVING RESTAURANTS>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+}
---
+++ src/test/java/RestaurantTest.java
@@ -0,0 +1,123 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+
+class RestaurantTest {
+    Restaurant restaurant;
+    //REFACTOR ALL THE REPEATED LINES OF CODE
+    LocalTime openingTime = LocalTime.parse("10:30:00");
+    LocalTime closingTime = LocalTime.parse("22:00:00");
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>OPEN/CLOSED<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    //-------FOR THE 2 TESTS BELOW, YOU MAY USE THE CONCEPT OF MOCKING, IF YOU RUN INTO ANY TROUBLE
+    @Test
+    public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
+        //WRITE UNIT TEST CASE HERE
+        restaurant = Mockito.spy(new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime));
+        LocalTime currentMockedTime = LocalTime.parse("13:00:00");
+        Mockito.when(restaurant.getCurrentTime()).thenReturn(currentMockedTime);
+        assertEquals(true, restaurant.isRestaurantOpen());
+
+    }
+
+    @Test
+    public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
+        //WRITE UNIT TEST CASE HERE
+        restaurant = Mockito.spy(new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime));
+        LocalTime currentMockedTime = LocalTime.parse("23:00:00");
+        Mockito.when(restaurant.getCurrentTime()).thenReturn(currentMockedTime);
+        assertEquals(false, restaurant.isRestaurantOpen());
+
+    }
+
+    //<<<<<<<<<<<<<<<<<<<<<<<<<OPEN/CLOSED>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>MENU<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    @Test
+    public void adding_item_to_menu_should_increase_menu_size_by_1(){
+
+        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.addToMenu("Sizzling brownie",319);
+        assertEquals(initialMenuSize+1,restaurant.getMenu().size());
+    }
+    @Test
+    public void removing_item_from_menu_should_decrease_menu_size_by_1() throws itemNotFoundException {
+
+        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.removeFromMenu("Vegetable lasagne");
+        assertEquals(initialMenuSize-1,restaurant.getMenu().size());
+    }
+    @Test
+    public void removing_item_that_does_not_exist_should_throw_exception() {
+
+        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+
+        assertThrows(itemNotFoundException.class,
+                ()->restaurant.removeFromMenu("French fries"));
+    }
+    //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+    // Adding feature using ADD
+    @Test
+    public void adding_no_items_to_order_display_total_value_as_0() throws itemNotFoundException {
+        restaurant =new Restaurant("Cafe coffee","Toronto",openingTime,closingTime);
+        restaurant.addToMenu("ItemA",110);
+        restaurant.addToMenu("ItemB", 220);
+
+        List<String> itemNames = new ArrayList<String>();
+
+        int totalPrice = restaurant.calculateTotalPrice(itemNames);
+        assertEquals(0, totalPrice);
+    }
+    @Test
+    public void adding_items_to_order_display_total_value_as_their_individual_price_summation() throws itemNotFoundException {
+        restaurant =new Restaurant("vogue cafe","Kerala",openingTime,closingTime);
+        restaurant.addToMenu("ItemA",110);
+        restaurant.addToMenu("ItemB", 220);
+        restaurant.addToMenu("ItemC", 330);
+
+        List<String> itemNames = new ArrayList<String>();
+        itemNames.add("ItemA");
+        itemNames.add("ItemC");
+
+        int totalPrice = restaurant.calculateTotalPrice(itemNames);
+        assertEquals(440, totalPrice);
+    }
+
+    @Test
+    public void adding_items_to_order_which_is_not_in_restaurant_menu_should_throw_an_exception() throws itemNotFoundException {
+        restaurant =new Restaurant("Bulls cafe","Kerala",openingTime,closingTime);
+        restaurant.addToMenu("ItemA",110);
+        restaurant.addToMenu("ItemB", 220);
+        restaurant.addToMenu("ItemC", 330);
+
+        List<String> itemNames = new ArrayList<String>();
+        itemNames.add("ItemE");
+
+        assertThrows(itemNotFoundException.class,
+                ()->restaurant.calculateTotalPrice(itemNames));
+    }
+}
